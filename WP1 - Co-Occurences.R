e2ce0393972a0ba4bda93bd50e35d4b757a7b543
Erik Kusch <erik@i-solution.de>
2020-05-29 16:29:39 +0200

    Test for Biome in Realm mapping

--- WP1 - Co-Occurences.R
+++ WP1 - Co-Occurences.R
@@ -2,7 +2,7 @@
 # PROJECT: [BFTP] Identifying Biomes And Their Shifts Using Remote Sensing
 # CONTENTS: Functionality to identify clusters of NDVI mean and seasonality
 # AUTHOR: Erik Kusch
-# EDIT: 22/05/20
+# EDIT: 29/05/20
 # ####################################################################### #
 rm(list=ls())
 ####### PREAMBLE/SOURCING ---------------------------------------------------------
@@ -109,60 +109,118 @@ if(!file.exists(file.path(Dir.Shapes, "WWF_ecoregions"))){
   unzip(file.path(Dir.Shapes, "wwf_ecoregions.zip"), exdir = file.path(Dir.Shapes, "WWF_ecoregions"))
 }
 EcoregionsMask <- readOGR(file.path(Dir.Shapes, "WWF_ecoregions", "official", "wwf_terr_ecos.shp"), verbose = FALSE) # loading shapefile for wwf ecoregions
+### vectors for WWF region naming
+Abbr_Realms <- levels((EcoregionsMask$REALM))
+Full_Realms <- c("Australasia", "Antarctic", "Afrotropics", "IndoMalay", "Nearctic", "Neotropics", "Oceania", "Palearctic")
+Abbr_Biomes <- 1:14
+Full_Biomes <- c("Tropical & Subtropical Moist Broadleaf Forests",
+                 "Tropical & Subtropical Dry Broadleaf Forests",
+                 "Tropical & Subtropical Coniferous Forests",
+                 "Temperate Broadleaf & Mixed Forests",
+                 "Temperate Conifer Forests",
+                 "Boreal Forests/Taiga",
+                 "Tropical & Subtropical Grasslands, Savannas & Shrublands",
+                 "Temperate Grasslands, Savannas & Shrublands",
+                 "Flooded Grasslands & Savannas",
+                 "Montane Grasslands & Shrublands",
+                 "Tundra",
+                 "Mediterranean Forests, Woodlands & Scrub",
+                 "Deserts & Xeric Shrublands",
+                 "Mangroves")
 
 ############## RANGE LOADING -------------------------------------------------
-## IUCN ----
-Dirs_vec <- c(Dir.Ranges.Amphibians, Dir.Ranges.Reptiles, Dir.Ranges.Mammals) # IUCN directories
-Ranges_ls <- as.list(rep(NA, length(Dirs_vec))) # List for range shapefiles in R
-Rasters_ls <- as.list(rep(NA, length(Dirs_vec))) # List for richness rasters in R
-for(Ranges_Iter in 1:length(Dirs_vec)){ # loop over all IUCN directories
-  Name <- strsplit(x = Dirs_vec[Ranges_Iter], split = paste0(Dir.Ranges, "/"))[[1]][2] # isolate name of directory
-  Shp <- readOGR(dsn = Dirs_vec[Ranges_Iter], verbose = TRUE) # load range shape data
-  Shp <- Shp[which(Shp$terrestial == "true"), ] # mask for terrestrial class
-  Ranges_ls[[Ranges_Iter]] <- Shp # save shapefiles to list
-  if(!file.exists(file.path(Dir.Ranges, paste0("Richness_", Name, ".nc")))){ # if richness raster has not been established yet
-    RichRas <- fasterize(st_as_sf(Shp), raster = Reference_ras, field = NULL, fun = "count") # establish richness raster
-    RichRas <- mask(RichRas, LandMask) # maks for land mask 
-    RichRas <- mask(RichRas, LakeMask, inverse = TRUE) # mask for lakes
-    writeRaster(x = RichRas, filename = file.path(Dir.Ranges, paste0("Richness_", Name)), format="CDF") # write raster with directory name to range directory
-  }else{ # if richness raster already exists
-    RichRas <-  raster(file.path(Dir.Ranges, paste0("Richness_", Name, ".nc"))) # load richness raster
+if(!file.exists(file.path(Dir.Ranges, "Ranges_ls.RData")) & !file.exists(file.path(Dir.Ranges, "Rasters_ls.RData"))){
+  ## IUCN ----
+  Dirs_vec <- c(Dir.Ranges.Amphibians, Dir.Ranges.Reptiles, Dir.Ranges.Mammals) # IUCN directories
+  Ranges_ls <- as.list(rep(NA, length(Dirs_vec))) # List for range shapefiles in R
+  Rasters_ls <- as.list(rep(NA, length(Dirs_vec))) # List for richness rasters in R
+  for(Ranges_Iter in 1:length(Dirs_vec)){ # loop over all IUCN directories
+    Name <- strsplit(x = Dirs_vec[Ranges_Iter], split = paste0(Dir.Ranges, "/"))[[1]][2] # isolate name of directory
+    Shp <- readOGR(dsn = Dirs_vec[Ranges_Iter], verbose = TRUE) # load range shape data
+    Shp <- Shp[which(Shp$terrestial == "true"), ] # mask for terrestrial class
+    Ranges_ls[[Ranges_Iter]] <- Shp # save shapefiles to list
+    if(!file.exists(file.path(Dir.Ranges, paste0("Richness_", Name, ".nc")))){ # if richness raster has not been established yet
+      RichRas <- fasterize(st_as_sf(Shp), raster = Reference_ras, field = NULL, fun = "count") # establish richness raster
+      RichRas <- mask(RichRas, LandMask) # maks for land mask 
+      RichRas <- mask(RichRas, LakeMask, inverse = TRUE) # mask for lakes
+      writeRaster(x = RichRas, filename = file.path(Dir.Ranges, paste0("Richness_", Name)), format="CDF") # write raster with directory name to range directory
+    }else{ # if richness raster already exists
+      RichRas <-  raster(file.path(Dir.Ranges, paste0("Richness_", Name, ".nc"))) # load richness raster
+    }
+    Rasters_ls[[Ranges_Iter]] <- RichRas # save richness raster to list
   }
-  Rasters_ls[[Ranges_Iter]] <- RichRas # save richness raster to list
+  names(Ranges_ls) <- gsub(pattern = paste0(Dir.Ranges, "/"), replacement = "", x = Dirs_vec) # set names of list
+  names(Rasters_ls) <- gsub(pattern = paste0(Dir.Ranges, "/"), replacement = "", x = Dirs_vec) # set names of list
+  rm(list = c("Dirs_vec", "Name", "Shp", "RichRas", "Ranges_Iter")) # clean environment
+  
+  ## BIRDLIFE ----
+  Birds_shp <- sf::st_read(dsn = file.path(Dir.Ranges.Birds, "BOTW.gdb")) # load BirdLife data
+  classes <- class(Birds_shp$Shape[1])[[1]] # read class of first shape object
+  for(Classes_Iter in 2:length(Birds_shp$Shape)){ # loop over all shapes in the shapefile
+    classes <- c(classes, class(Birds_shp$Shape[Classes_Iter])[[1]]) # append the class of current object
+  }
+  Birds_shp <- Birds_shp[which(classes == "sfc_MULTIPOLYGON"), ] # retain MULTIPOLYGON objects, this gets rid of MULTISURFACE objects which fasterize can't handle
+  if(!file.exists(file.path(Dir.Ranges, "Richness_BIRDS.nc"))){ # if richness raster has not been established yet
+    Birdsrich_ras <- fasterize(Birds_shp, raster = Reference_ras, field = NULL, fun = "count") # establish richness raster
+    Birdsrich_ras <- mask(Birdsrich_ras, LandMask) # maks for land mask 
+    Birdsrich_ras <- mask(Birdsrich_ras, LakeMask, inverse = TRUE) # mask for lakes
+    writeRaster(x = Birdsrich_ras, filename = file.path(Dir.Ranges, "Richness_BIRDS"), format="CDF") # write raster with
+  }else{ # if richness raster already exists
+    Birdsrich_ras <-  raster(file.path(Dir.Ranges, paste0("Richness_BIRDS.nc"))) # load richness raster
+  }
+  
+  ## BIEN ----
+  
+  
+  ## SAVING TO DISK ----
+  Ranges_ls[["BIRDS"]] <- Birds_shp # save to list
+  save(Ranges_ls, file = file.path(Dir.Ranges, "Ranges_ls.RData"))
+  Rasters_ls[["Total"]] <- sum(stack(Rasters_ls), na.rm = TRUE) # calculate total species richness
+  Rasters_ls[["Total"]] <- mask(Rasters_ls[["Total"]], LandMask) # maks for land mask 
+  Rasters_ls[["Total"]] <- mask(Rasters_ls[["Total"]], LakeMask, inverse = TRUE) # mask for lakes
+  Rasters_ls[["BIRDS"]] <- Birdsrich_ras # save to list
+  save(Rasters_ls, file = file.path(Dir.Ranges, "Rasters_ls.RData"))
+  rm(list = c("Birds_shp", "Birdsrich_ras", "classes", "Classes_Iter")) # cleaning up environment
+}else{
+  load(file.path(Dir.Ranges, "Ranges_ls.RData"))
+  load(file.path(Dir.Ranges, "Rasters_ls.RData"))
 }
-names(Ranges_ls) <- gsub(pattern = paste0(Dir.Ranges, "/"), replacement = "", x = Dirs_vec) # set names of list
-names(Rasters_ls) <- gsub(pattern = paste0(Dir.Ranges, "/"), replacement = "", x = Dirs_vec) # set names of list
-rm(list = c("Dirs_vec", "Name", "Shp", "RichRas", "Ranges_Iter")) # clean environment
-
-## BIRDLIFE ----
-Birds_shp <- sf::st_read(dsn = file.path(Dir.Ranges.Birds, "BOTW.gdb")) # load BirdLife data
-classes <- class(Birds_shp$Shape[1])[[1]] # read class of first shape object
-for(Classes_Iter in 2:length(Birds_shp$Shape)){ # loop over all shapes in the shapefile
-  classes <- c(classes, class(Birds_shp$Shape[Classes_Iter])[[1]]) # append the class of current object
-}
-Birds_shp <- Birds_shp[which(classes == "sfc_MULTIPOLYGON"), ] # retain MULTIPOLYGON objects, this gets rid of MULTISURFACE objects which fasterize can't handle
-if(!file.exists(file.path(Dir.Ranges, "Richness_BIRDS.nc"))){ # if richness raster has not been established yet
-  Birdsrich_ras <- fasterize(Birds_shp, raster = Reference_ras, field = NULL, fun = "count") # establish richness raster
-  Birdsrich_ras <- mask(Birdsrich_ras, LandMask) # maks for land mask 
-  Birdsrich_ras <- mask(Birdsrich_ras, LakeMask, inverse = TRUE) # mask for lakes
-  writeRaster(x = Birdsrich_ras, filename = file.path(Dir.Ranges, "Richness_BIRDS"), format="CDF") # write raster with
-}else{ # if richness raster already exists
-  Birdsrich_ras <-  raster(file.path(Dir.Ranges, paste0("Richness_BIRDS.nc"))) # load richness raster
-}
-Ranges_ls[["BIRDS"]] <- Birds_shp # save to list
-Rasters_ls[["BIRDS"]] <- Birdsrich_ras # save to list
-rm(list = c("Birds_shp", "Birdsrich_ras", "classes", "Classes_Iter")) # cleaning up environment
-
-## BIEN ----
-
 
 ############## PLOTTING RICHNESS MAPS ----------------------------------------
-Rasters_ls[["Total"]] <- sum(stack(Rasters_ls), na.rm = TRUE) # build stack
-Rasters_ls[["Total"]] <- mask(Rasters_ls[["Total"]], LandMask) # maks for land mask 
-Rasters_ls[["Total"]] <- mask(Rasters_ls[["Total"]], LakeMask, inverse = TRUE) # mask for lakes
 ## MAPVIEW HTML ----
+
+### PICK IT UP HERE!! - SHOW BIOMES AND REALMS AND PROTECTED AREAS ON THE MAP AS WELL AS RIVERS
+
+Ecoregions_sf <- st_as_sf(EcoregionsMask) 
+
+
+library(dplyr)
+
+Realms_sf <- Ecoregions_sf %>% 
+  group_by(REALM) %>%
+  summarise(geometry = sf::st_union(geometry)) %>%
+  ungroup()
+Realms_sf <- Realms_sf[which(Realms_sf$REALM %in% Abbr_Realms), ]
+Realms_sf$REALM <- Full_Realms
+
+Biomes_sf <- Ecoregions_sf %>% 
+  group_by(BIOME) %>%
+  summarise(geometry = sf::st_union(geometry)) %>%
+  ungroup()
+Biomes_sf <- Biomes_sf[which(Biomes_sf$BIOME %in% Abbr_Biomes), ]
+Biomes_sf$BIOME <- Full_Biomes
+
+# Union_sf <- st_union(Realms_sf, Biomes_sf)
+RealmBiomes_sf <- st_intersection(Realms_sf, Biomes_sf)
+RealmBiomes_sf$REALMBIOMES <- paste(RealmBiomes_sf$REALM, RealmBiomes_sf$BIOME, sep=" - ")
+RealmBiomes_sf <- RealmBiomes_sf[, c(-1,-2)]
+
 # ProtectedAreas_mv <- mapview(ProtectedAreasMask, col.regions = "green", color = "black", alpha.regions = 0.3)
-Countries_mv <- mapview(CountryMask, color = "black", alpha.regions = 0)
+# Countries_mv <- mapview(CountryMask, color = "black", alpha.regions = 0)
+Rivers_mv <- mapview(RiversMask, color = "blue", alpha.regions = 0)
+# RealmBiomes_mv <- mapview(RealmBiomes_sf, zcol = "REALM", labels = paste(Intersec$REALM, Intersec$BIOME, sep=" - "))
+RealmBiomes_mv <- mapview(RealmBiomes_sf, color = "black")
+# Biomes_mv <- mapview(Biomes_sf, color = "red", legend = FALSE, alpha.regions = 0)
 Amphibians_mv <- mapview(layer.name = "Amphibian Species Richness", Rasters_ls$AMPHIBIANS, legend = TRUE, 
                          maxpixels =  ncell(Reference_ras), na.color = "#FFFFFF00") 
 Reptiles_mv <- mapview(layer.name = "Reptilian Species Richness", Rasters_ls$REPTILES, legend = TRUE, 
@@ -195,30 +253,34 @@ for(Plot_Iter in 1:length(Rasters_ls)){
 rm(list = c("Plot_Iter", "Title_vec", "col.mapview"))
 
 ####### SPECIES LIST -----------------------------------------------------
-SpeciesNames_vec <- NA
-DataSets_vec <- NA
-for(Names_Iter in 1:length(Ranges_ls)){ # loop over all range data sets
-  if(names(Ranges_ls)[[Names_Iter]] != "BIRDS"){ # BIRD data is stored differently than IUCN data
-    SpeciesNamesAdd_vec <- as.character(Ranges_ls[[Names_Iter]]$binomial) # extract species names
-  }else{
-    SpeciesNamesAdd_vec <- as.character(Ranges_ls[[Names_Iter]]$SCINAME) # extract species names
+if(!file.exists(file.path(Dir.Ranges, "Species_df.RData"))){
+  SpeciesNames_vec <- NA
+  DataSets_vec <- NA
+  for(Names_Iter in 1:length(Ranges_ls)){ # loop over all range data sets
+    if(names(Ranges_ls)[[Names_Iter]] != "BIRDS"){ # BIRD data is stored differently than IUCN data
+      SpeciesNamesAdd_vec <- as.character(Ranges_ls[[Names_Iter]]$binomial) # extract species names
+    }else{
+      SpeciesNamesAdd_vec <- as.character(Ranges_ls[[Names_Iter]]$SCINAME) # extract species names
+    }
+    SpeciesNames_vec <- c(SpeciesNames_vec, SpeciesNamesAdd_vec) # combine species names
+    DataSets_vec <- c(DataSets_vec, rep(Names_Iter, length(SpeciesNamesAdd_vec))) # note range data set (i.e. place in Ranges_ls)
   }
-  SpeciesNames_vec <- c(SpeciesNames_vec, SpeciesNamesAdd_vec) # combine species names
-  DataSets_vec <- c(DataSets_vec, rep(Names_Iter, length(SpeciesNamesAdd_vec))) # note range data set (i.e. place in Ranges_ls)
+  # Make Species data frame listing names, IDs, and data sets
+  Species_df <- data.frame(ID = rep(NA, length(DataSets_vec)),
+                           Name = SpeciesNames_vec,
+                           Group = DataSets_vec)
+  Species_df <- Species_df[-1, ] # remove initial NA row
+  Species_df <- Species_df[!duplicated(Species_df), ] # remove multiple mentions of the same species
+  Species_df <- Species_df[order(Species_df$Name),] # sort by alphabet
+  Species_df$ID <- 1:dim(Species_df)[1] # Species ID for later analysis
+  save(Species_df, file = file.path(Dir.Ranges, "Species_df.RData"))
+  rm(list = c("DataSets_vec", "SpeciesNames_vec", "Names_Iter"))
+}else{
+  load(file.path(Dir.Ranges, "Species_df.RData"))
 }
-# Make Species data frame listing names, IDs, and data sets
-Species_df <- data.frame(ID = rep(NA, length(DataSets_vec)),
-                         Name = SpeciesNames_vec,
-                         Group = DataSets_vec)
-Species_df <- Species_df[-1, ] # remove initial NA row
-Species_df <- Species_df[!duplicated(Species_df), ] # remove multiple mentions of the same species
-Species_df <- Species_df[order(Species_df$Name),] # sort by alphabet
-Species_df$ID <- 1:dim(Species_df)[1] # Species ID for later analysis
-
-rm("DataSets_vec")
 
 ####### ESTABLISH CO-OCCURRENCES -----------------------------------------------------
-### OCCURRENCE DATA FRAME BY REALMS/BIOMES ----
+### OCCURRENCE DATA FRAME BIOMES IN REALMS ----
 if(!file.exists(file.path(Dir.Ranges, "SpeciesCells_df.RData"))){
   if(!file.exists(file.path(Dir.Ranges, "SpeciesCells_ls.RData"))){
     Realms_ras <- fasterize(sf = st_as_sf(EcoregionsMask), raster = Reference_ras, field = "REALM", fun = "first")
@@ -260,7 +322,7 @@ if(!file.exists(file.path(Dir.Ranges, "SpeciesCells_df.RData"))){
   load(file.path(Dir.Ranges, "SpeciesCells_df.RData"))
 }
 
-### OCCURRENCE DATA FRAME BY BIOMES IN REALMS ----
+### COMMUNITY DATA FRAME BY BIOMES IN REALMS ----
 if(!file.exists(file.path(Dir.Ranges, "SpeciesCells_df.RData"))){
   BiomeRealms <- sort(unique(SpeciesCells_df$BiomesInRealms))
   SiteCommunities_ls <- as.list(rep(NA, length(BiomeRealms)))
@@ -288,21 +350,5 @@ plot_netassoc_network(assoc_net$network_all)
 
 
 ### OUTPUT ----
-Abbr_Realms <- 1:8
-Full_Realms <- c("Australasia", "Antarctic", "Afrotropics", "IndoMalay", "Nearctic", "Neotropics", "Oceania", "Palearctic")
-Abbr_Biomes <- 1:18
-Full_Biomes <- c("Tropical & Subtropical Moist Broadleaf Forests",
-                 "Tropical & Subtropical Dry Broadleaf Forests",
-                 "Tropical & Subtropical Coniferous Forests",
-                 "Temperate Broadleaf & Mixed Forests",
-                 "Temperate Conifer Forests",
-                 "Boreal Forests/Taiga",
-                 "Tropical & Subtropical Grasslands, Savannas & Shrublands",
-                 "Temperate Grasslands, Savannas & Shrublands",
-                 "Flooded Grasslands & Savannas",
-                 "Montane Grasslands & Shrublands",
-                 "Tundra",
-                 "Mediterranean Forests, Woodlands & Scrub",
-                 "Deserts & Xeric Shrublands",
-                 "Mangroves")
+
 
