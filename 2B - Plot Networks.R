b83b25abd4eee34030abfbdf8ac2fdbb837c8acc
Erik Kusch (Tower PC) <erik@i-solution.de>
2020-11-25 17:38:38 +0100

    Initial network inspection

--- 2B - Plot Networks.R
+++ 2B - Plot Networks.R
@@ -144,6 +144,9 @@ library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores()) 
 
+# install.packages(c("coda","mvtnorm","devtools","loo","dagitty"))
+# Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true")
+# devtools::install_github("rmcelreath/rethinking")
 library(rethinking)
 library(reshape2)
 
@@ -178,4 +181,49 @@ param.vec <- c('disp_dev', 'a', 'interactions','effect', 'response', 'sigma_alph
 
 
 
+fit <- readRDS(file.path(Dir.Base, "Y - Shared/Malyon", "FIA_InterNet.rds"))
+library(igraph)
 
+
+source(file.path(Dir.Base, "5 - Collaborators & Contributions/Interaction Network Method (Malyon Bimler)", "return_inter_array.R"))
+
+
+# Get the full posteriors 
+joint.post.draws <- extract.samples(fit)
+
+# Select parameters of interest
+param.vec <- c('a', 'beta_ij', 'effect', 'response', 're', 'inter_mat',
+               'mu', 'disp_dev', 'sigma_alph')
+
+# Draw 1000 samples from the 80% posterior interval for each parameter of interest
+p.samples <- list()
+p.samples <- sapply(param.vec[param.vec != 'sigma_alph' & param.vec != 'inter_mat'], function(p) {
+  p.samples[[p]] <- apply(joint.post.draws[[p]], 2, function(x){
+    sample(x[x > quantile(x, 0.1) & x < quantile(x, 0.9)], size = 1000)
+  })  # this only works for parameters which are vectors
+})
+# there is only one sigma_alph parameter so we must sample differently:
+p.samples[['sigma_alph']] <- sample(joint.post.draws$sigma_alph[
+  joint.post.draws$sigma_alph > quantile(joint.post.draws$sigma_alph, 0.1) & 
+    joint.post.draws$sigma_alph < quantile(joint.post.draws$sigma_alph, 0.9)], size = 1000)
+
+# WARNING: in the STAN model for annual wildflowers, parameter 'a' lies within an exponential,
+# 'a' estimates must thus be exponentiated to return estimates of intrinsic performance
+intrinsic.perf <- exp(p.samples$a)
+colnames(intrinsic.perf) <- focalID
+
+# Build matrices of interaction estimates
+#----------------------------------------
+inter_mat <- return_inter_array(joint.post.draws, 
+                                response = p.samples$response,
+                                effect = p.samples$effect,
+                                focalID,
+                                neighbourID)
+# inter_mat is now a 3 dimensional array, where rows = focals, columns = neighbours and 3rd dim = samples from the posterior
+# inter_mat[ , , 1] should return a matrix consisting of one sample for every interaction 
+# apply(inter_mat, c(1, 2), mean) will return the mean estimate for every interaction (NB: this is the 
+# mean of the 80% posterior interval, so will be slightly different to the mean value returned from 
+# summary(fit), which is calculated from the full posterior distribution) 
+
+# Interactions can now be divided by the appropriate scaling (intrinsic performance, and demographic rates
+# if a population dynamics model is used) in order to return per capita interaction strengths. 
