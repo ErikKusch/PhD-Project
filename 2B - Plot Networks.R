0ae72ed495acad18863c8e099c6be5cbc7c74bcd
Erik Kusch <au656028@uni.au.dk>
2020-12-07 12:38:55 +0100

    Server commit

--- 2B - Plot Networks.R
+++ 2B - Plot Networks.R
@@ -23,9 +23,9 @@ FUN_PlotData_FIA <- function(states = c("DE","MD"), ByYear = FALSE, nCores = par
   }
   
   FIA_df <- biomass(db = FIA_df, # which data base to use
-                   bySpecies = TRUE, # group by Species
-                   byPlot = TRUE, # group by plot
-                   nCores = nCores # use half the machines cores for this
+                    bySpecies = TRUE, # group by Species
+                    byPlot = TRUE, # group by plot
+                    nCores = nCores # use half the machines cores for this
   )
   
   FIA_df <- FIA_df[,c("pltID", "BIO_ACRE", "SCIENTIFIC_NAME", "nStems", "YEAR")] # select columns we need
@@ -66,7 +66,6 @@ FUN_PlotData_FIA <- function(states = c("DE","MD"), ByYear = FALSE, nCores = par
   }
   return(Interaction_df)
 }
-
 
 FIA_df <- FUN_PlotData_FIA(states = c("DE","MD"), ByYear = TRUE, nCores = parallel::detectCores()/2)
 FIA_df <- FIA_df[-c(which(FIA_df$biomass == 0)),]# remove 0 biomass entries
@@ -131,9 +130,10 @@ FUN_NetInter_Prep <- function(Fitness = "biomass", data = NULL){
   return(stan.data)
 }
 
-
-FIA_StanList <- FUN_NetInter_Prep(Fitness = "biomass", data = FIA_df)
-
+FIA_StanList <- FUN_NetInter_Prep(Fitness = "biomass", data = FIA_df) # for some reason, this doesn't work correctly on the server... old R-version? 
+# str(FIA_StanList)
+# saveRDS(object = FIA_StanList, file = file.path(Dir.Plots, "STANLISTFIA.rds")) # to be executed on home PC
+# FIA_StanList <- readRDS(file = file.path(Dir.Plots, "STANLISTFIA.rds")) # to be executed on server
 
 ########################################################################################################
 
@@ -144,6 +144,9 @@ library(rstan)
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores()) 
 
+# install.packages(c("coda","mvtnorm","devtools","loo","dagitty"))
+# Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true")
+# devtools::install_github("rmcelreath/rethinking")
 library(rethinking)
 library(reshape2)
 
@@ -165,7 +168,7 @@ fit <- stan(file = 'Supplement - StanModel.stan',
             chains = 4,
             warmup = 1000,          # number of warmup iterations per chain
             iter = 5000,            # total number of iterations per chain
-            refresh = 50,         # show progress every 'refresh' iterations
+            refresh = 100,         # show progress every 'refresh' iterations
             control = list(max_treedepth = 10)
 )
 # You will probably get a few error warnings - including one that says certain chains won't have converged, 
@@ -178,4 +181,49 @@ param.vec <- c('disp_dev', 'a', 'interactions','effect', 'response', 'sigma_alph
 
 
 
+fit <- readRDS(file.path(Dir.Base, "Y - Shared/Malyon", "FIA_InterNet.rds"))
+library(igraph)
 
+
+source(file.path(Dir.Base, "5 - Collaborators & Contributions/Interaction Network Method (Malyon Bimler)", "return_inter_array.R"))
+
+
+# Get the full posteriors 
+joint.post.draws <- extract.samples(fit)
+
+# Select parameters of interest
+param.vec <- c('a', 'beta_ij', 'effect', 'response', 're', 'inter_mat',
+               'mu', 'disp_dev', 'sigma_alph')
+
+# Draw 1000 samples from the 80% posterior interval for each parameter of interest
+p.samples <- list()
+p.samples <- sapply(param.vec[param.vec != 'sigma_alph' & param.vec != 'inter_mat'], function(p) {
+  p.samples[[p]] <- apply(joint.post.draws[[p]], 2, function(x){
+    sample(x[x > quantile(x, 0.1) & x < quantile(x, 0.9)], size = 1000)
+  })  # this only works for parameters which are vectors
+})
+# there is only one sigma_alph parameter so we must sample differently:
+p.samples[['sigma_alph']] <- sample(joint.post.draws$sigma_alph[
+  joint.post.draws$sigma_alph > quantile(joint.post.draws$sigma_alph, 0.1) & 
+    joint.post.draws$sigma_alph < quantile(joint.post.draws$sigma_alph, 0.9)], size = 1000)
+
+# WARNING: in the STAN model for annual wildflowers, parameter 'a' lies within an exponential,
+# 'a' estimates must thus be exponentiated to return estimates of intrinsic performance
+intrinsic.perf <- exp(p.samples$a)
+colnames(intrinsic.perf) <- focalID
+
+# Build matrices of interaction estimates
+#----------------------------------------
+inter_mat <- return_inter_array(joint.post.draws, 
+                                response = p.samples$response,
+                                effect = p.samples$effect,
+                                focalID,
+                                neighbourID)
+# inter_mat is now a 3 dimensional array, where rows = focals, columns = neighbours and 3rd dim = samples from the posterior
+# inter_mat[ , , 1] should return a matrix consisting of one sample for every interaction 
+# apply(inter_mat, c(1, 2), mean) will return the mean estimate for every interaction (NB: this is the 
+# mean of the 80% posterior interval, so will be slightly different to the mean value returned from 
+# summary(fit), which is calculated from the full posterior distribution) 
+
+# Interactions can now be divided by the appropriate scaling (intrinsic performance, and demographic rates
+# if a population dynamics model is used) in order to return per capita interaction strengths. 
