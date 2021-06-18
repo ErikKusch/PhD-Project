df1146ba71556cb63d6cf2b29d2314b611ca7e7e
Erik Kusch <au656028@uni.au.dk>
2021-06-18 12:54:57 +0200

    Itteration of Bimler method over different treatments and plots of PFTC data

--- 2A - PFTC Networks.R
+++ 2A - PFTC Networks.R
@@ -313,161 +313,175 @@ stan_post_pred_check <- function(post.draws,
 }
 
 ################# RUN NETWORK METHOD  ########################################### --------------------------
-if(file.exists(file.path(Dir.PlotNets.PFTC, "Model.RData"))){
-  stop(paste("PFTC models already run. You can find them here:"), Dir.PlotNets.PFTC)
+Treatments_df <- data.frame(Treatment = sapply(str_split(Mal_df$PlotID, "_"), "[[", 2),
+                            Plot = sapply(str_split(Mal_df$PlotID, "_"), "[[", 1)
+)
+Treatments_vec <- c("ALL", as.character(unique(Treatments_df$Treatment)), as.character(unique(Treatments_df$Plot)))           
+
+for(Treatment in Treatments_vec){
+  
+  if(Treatment != "ALL"){
+    Mal_df <- Mal_df[which(Treatments_df[,1] == Treatment | Treatments_df[,2] == Treatment), ]
+  }
+  
+  Dir.PlotNets.PFTC.Iter <- file.path(Dir.PlotNets.PFTC, Treatment)
+  if(dir.exists(Dir.PlotNets.PFTC.Iter)){
+    stop(paste("PFTC models already run for Treatment/Plot, ", Treatment,". You can find them here:"), Dir.PlotNets.PFTC.Iter)
+  }
+  dir.create(Dir.PlotNets.PFTC.Iter)
+  Mal_df[is.na(Mal_df)] <- 0
+  OmitCols <- -(which(colnames(Mal_df)[-c(1:4)] %nin% Mal_df$focal)+4)
+  if(length(OmitCols) != 0){
+    Mal_df <- Mal_df[, OmitCols]
+  }else{
+    Mal_df <- Mal_df
+  }
+  Mal_df <- Mal_df[Mal_df$fit != 0, ]
+  
+  FIA_StanList <- Fun_StanList(Fitness = "fit", data = Mal_df)
+  
+  #### Preferences
+  rstan_options(auto_write = TRUE)
+  rstan_options(javascript = FALSE)
+  options(mc.cores = nChains) 
+  
+  ## CHECKING DATA
+  focalID <- unique(Mal_df$focal)
+  neighbourID <- colnames(Mal_df[ , -c(1:4)])
+  Fun_PreCheck(data = Mal_df)
+  ## RUNNING MODEL
+  fit <- stan(file = 'Supplement - StanModel.stan',
+              data =  FIA_StanList,               # named list of data
+              chains = nChains,
+              warmup = nWarmup,          # number of warmup iterations per chain
+              iter = nSamples,            # total number of iterations per chain
+              refresh = 100,         # show progress every 'refresh' iterations
+              control = list(max_treedepth = 10)
+  )
+  save(fit, file = file.path(Dir.PlotNets.PFTC.Iter, "Model.RData"))
+  
+  ## MODEL DIAGNOSTICS
+  # Get the full posteriors 
+  joint.post.draws <- extract.samples(fit)
+  # Select parameters of interest
+  param.vec <- c('a', 'beta_ij', 'effect', 'response', 're', 'inter_mat', 'mu', 'sigma_alph'
+                 # 'disp_dev' # including this one leads to "Error in apply(joint.post.draws[[p]], 2, function(x) { : dim(X) must have a positive length"
+  )
+  # Draw 1000 samples from the 80% posterior interval for each parameter of interest
+  p.samples <- list()
+  p.samples <- sapply(param.vec[param.vec != 'sigma_alph' & param.vec != 'inter_mat'], function(p) {
+    p.samples[[p]] <- apply(joint.post.draws[[p]], 2, function(x){
+      sample(x[x > quantile(x, 0.1) & x < quantile(x, 0.9)], size = 1000)
+    })  # this only works for parameters which are vectors
+  })
+  # there is only one sigma_alph parameter so we must sample differently:
+  p.samples[['sigma_alph']] <- sample(joint.post.draws$sigma[
+    joint.post.draws$sigma > quantile(joint.post.draws$sigma, 0.1) & 
+      joint.post.draws$sigma < quantile(joint.post.draws$sigma, 0.9)], size = 1000)
+  # WARNING: in the STAN model, parameter 'a' lies within a logarithmic, and must thus be logarithmitised to return estimates of intrinsic performance
+  intrinsic.perf <- log(p.samples$a)
+  colnames(intrinsic.perf) <- focalID
+  inter_mat <- return_inter_array(joint.post.draws, 
+                                  response = p.samples$response,
+                                  effect = p.samples$effect,
+                                  sort(focalID),
+                                  neighbourID)
+  # inter_mat is now a 3 dimensional array, where rows = focals, columns = neighbours and 3rd dim = samples from the posterior; inter_mat[ , , 1] should return a matrix consisting of one sample for every interaction 
+  param.vec <- c('a', 'beta_ij', 'effect', 'response', 're', 'inter_mat', 'mu', 'sigma')
+  try(stan_model_check(fit = fit,
+                       results_folder = Dir.PlotNets.PFTC.Iter,
+                       params = param.vec))
+  # stan_post_pred_check(post.draws = joint.post.draws, # error here on missing data!!!
+  #                       results_folder = Dir.Malyon,
+  #                       stan.data = StanList_ls)
+  # Interactions can now be divided by the appropriate scaling (intrinsic performance, and demographic rates
+  # if a population dynamics model is used) in order to return per capita interaction strengths. 
+  Interaction_mean <- apply(inter_mat, c(1, 2), mean) # will return the mean estimate for every interaction (NB: this is the mean of the 80% posterior interval, so will be slightly different to the mean value returned from summary(fit), which is calculated from the full posterior distribution)  
+  Interaction_mean <- Interaction_mean[, order(colnames(Interaction_mean))] # sort columns
+  Interaction_mean <- Interaction_mean*-1 # need to switch sign of results
+  diag(Interaction_mean) <- NA
+  Interaction_hpdi <- apply(inter_mat, c(1, 2), HPDI, prob = 0.89) *-1 # need to switch sign of results
+  Interaction_min <- Interaction_hpdi[1,,]
+  diag(Interaction_min) <- NA
+  Interaction_max <- Interaction_hpdi[2,,]
+  diag(Interaction_max) <- NA
+  Interactions_igraph <- data.frame(Actor = rep(dimnames(Interaction_mean)$neighbour, length(dimnames(Interaction_mean)$species)),
+                                    Subject = rep(dimnames(Interaction_mean)$species, each = length(dimnames(Interaction_mean)$neighbour)),
+                                    Inter_mean = as.vector(t(Interaction_mean)),
+                                    Inter_min = as.vector(t(Interaction_min)),
+                                    Inter_max = as.vector(t(Interaction_max))
+  )
+  # Interactions_Malyon <- Interactions_igraph[order(abs(Interactions_igraph$Inter_mean), decreasing = TRUE), ]
+  # Interactions_igraph <- na.omit(Interactions_Malyon)
+  
+  Interactions_DAG <- Interactions_igraph[order(Interactions_igraph$Actor), ]
+  Interactions_DAG <- na.omit(Interactions_DAG)
+  
+  Signs_DAG <- with(Interactions_DAG, data.frame(
+    Intrer_mean = sign(Inter_mean),
+    Intrer_max = sign(Inter_max),
+    Intrer_min = sign(Inter_min)
+  ))
+  
+  Interactions_DAG <- Interactions_DAG[abs(apply(Signs_DAG, 1, sum)) == 3, ]
+  
+  Interactions_DAG$Actor <- gsub(x = Interactions_DAG$Actor, pattern = " ", replacement = "_")
+  Interactions_DAG$Subject <- gsub(x = Interactions_DAG$Subject, pattern = " ", replacement = "_")
+  
+  
+  ## Graph Plot
+  Dag_Paths <- paste(paste(Interactions_DAG$Actor, "->", Interactions_DAG$Subject), collapse = " ")
+  dag <- dagitty(x = paste0("dag {", Dag_Paths, "}"))
+  tidy_dag <- tidy_dagitty(dag, layout = "fr")
+  tidy_dag$data$weight <- c(abs(Interactions_DAG$Inter_mean), rep(NA, sum(is.na(tidy_dag$data$direction))))
+  tidy_dag$data$label <- c(round(Interactions_DAG$Inter_mean, 2), rep(NA, sum(is.na(tidy_dag$data$direction))))
+  tidy_dag$data$colour <- c(ifelse(c(Interactions_DAG$Inter_mean) > 0, "green", "red"), rep("white", sum(is.na(tidy_dag$data$direction))))
+  WeightMod <- 1/max(tidy_dag$data$weight, na.rm = TRUE)
+  
+  DAG_plot <- ggplot(tidy_dag, aes(x = x, y = y, xend = xend, yend = yend), layout = "linear") +
+    geom_dag_point(colour = "grey", shape = 18, size = 10) +
+    geom_dag_text(colour = "black", size = 2) +
+    geom_dag_edges_arc(aes(edge_colour = colour, edge_width = exp(weight)*WeightMod)) +
+    # geom_dag_edges_diagonal(aes(edge_colour = colour, edge_width = weight*WeightMod)) + 
+    labs(title = Treatment, subtitle = "Realised Interaction (Bimler et al. Method") + 
+    theme_dag()
+  ggsave(DAG_plot, units = "cm", width = 32, height = 18, filename = file.path(Dir.PlotNets.PFTC.Iter, "DAG.png"))
+  
+  dag_data <- na.omit(tidy_dag$data)
+  df <- data.frame(
+    A = dag_data$name, 
+    B = dag_data$to)
+  df.g <- graph.data.frame(d = df, directed = TRUE)
+  E(df.g)$weight <- dag_data$weight
+  E(df.g)$label <- dag_data$label
+  E(df.g)$colour <- dag_data$colour
+  
+  igraph_plot <- ggraph(df.g, layout = 'linear', circular = TRUE) + 
+    # geom_edge_arc(aes(col = colour, width = weight*WeightMod, alpha = ..index..)) + 
+    # scale_edge_alpha('Edge direction', guide = 'edge_direction') + 
+    geom_edge_arc(aes(col = colour, width = exp(dag_data$weight)*WeightMod), show.legend = FALSE) + 
+    scale_edge_color_manual(values = rev(unique(dag_data$colour))) +
+    geom_node_point(color = "black", size = 2) + 
+    geom_node_text(aes(label = name),  repel = TRUE)+
+    coord_fixed() + 
+    labs(title = Treatment, subtitle = "Realised Interaction (Bimler et al. Method)") + 
+    theme_graph()
+  ggsave(igraph_plot, units = "cm", width = 32, height = 32, filename = file.path(Dir.PlotNets.PFTC.Iter, "IGraph.png"))
+  
+  diag(Interaction_mean) <- 0
+  Interaction_mean[abs(sign(Interaction_mean) + sign(Interaction_min) + sign(Interaction_max)) != 3] <- 0
+  
+  
+  
+  fv.colors = colorRampPalette(c("red","white","green")) ## define the color ramp
+  colorlut = fv.colors(100)[c(1,seq(50,100,length.out=99))] ## select colors to use
+  
+  seq(sum(Interaction_mean>0))
+  
+  jpeg(filename = file.path(Dir.PlotNets.PFTC.Iter, "Interactions.jpeg"), units = "cm", width = 32, height = 18, res = 1000)
+  pheatmap(Interaction_mean, display_numbers = T, 
+           color = c("red", "white","green"), 
+           breaks = c(min(Interaction_mean, na.rm = TRUE), -0.01, 0.01, max(Interaction_mean, na.rm = TRUE)), main = "Actors (Columns) x Subject (Rows)",
+           fontsize = 5)
+  dev.off()
 }
-Mal_df[is.na(Mal_df)] <- 0
-OmitCols <- -(which(colnames(Mal_df)[-c(1:4)] %nin% Mal_df$focal)+4)
-if(length(OmitCols) != 0){
-  Mal_df <- Mal_df[, OmitCols]
-}else{
-  Mal_df <- Mal_df
-}
-Mal_df <- Mal_df[Mal_df$fit != 0, ]
-
-FIA_StanList <- Fun_StanList(Fitness = "fit", data = Mal_df)
-
-#### Preferences
-rstan_options(auto_write = TRUE)
-rstan_options(javascript = FALSE)
-options(mc.cores = nChains) 
-
-## CHECKING DATA
-focalID <- unique(Mal_df$focal)
-neighbourID <- colnames(Mal_df[ , -c(1:4)])
-Fun_PreCheck(data = Mal_df)
-## RUNNING MODEL
-fit <- stan(file = 'Supplement - StanModel.stan',
-            data =  FIA_StanList,               # named list of data
-            chains = nChains,
-            warmup = nWarmup,          # number of warmup iterations per chain
-            iter = nSamples,            # total number of iterations per chain
-            refresh = 100,         # show progress every 'refresh' iterations
-            control = list(max_treedepth = 10)
-)
-save(fit, file = file.path(Dir.FIABiome, "Model.RData"))
-
-## MODEL DIAGNOSTICS
-# Get the full posteriors 
-joint.post.draws <- extract.samples(fit)
-# Select parameters of interest
-param.vec <- c('a', 'beta_ij', 'effect', 'response', 're', 'inter_mat', 'mu', 'sigma_alph'
-               # 'disp_dev' # including this one leads to "Error in apply(joint.post.draws[[p]], 2, function(x) { : dim(X) must have a positive length"
-)
-# Draw 1000 samples from the 80% posterior interval for each parameter of interest
-p.samples <- list()
-p.samples <- sapply(param.vec[param.vec != 'sigma_alph' & param.vec != 'inter_mat'], function(p) {
-  p.samples[[p]] <- apply(joint.post.draws[[p]], 2, function(x){
-    sample(x[x > quantile(x, 0.1) & x < quantile(x, 0.9)], size = 1000)
-  })  # this only works for parameters which are vectors
-})
-# there is only one sigma_alph parameter so we must sample differently:
-p.samples[['sigma_alph']] <- sample(joint.post.draws$sigma[
-  joint.post.draws$sigma > quantile(joint.post.draws$sigma, 0.1) & 
-    joint.post.draws$sigma < quantile(joint.post.draws$sigma, 0.9)], size = 1000)
-# WARNING: in the STAN model, parameter 'a' lies within a logarithmic, and must thus be logarithmitised to return estimates of intrinsic performance
-intrinsic.perf <- log(p.samples$a)
-colnames(intrinsic.perf) <- focalID
-inter_mat <- return_inter_array(joint.post.draws, 
-                                response = p.samples$response,
-                                effect = p.samples$effect,
-                                sort(focalID),
-                                neighbourID)
-# inter_mat is now a 3 dimensional array, where rows = focals, columns = neighbours and 3rd dim = samples from the posterior; inter_mat[ , , 1] should return a matrix consisting of one sample for every interaction 
-param.vec <- c('a', 'beta_ij', 'effect', 'response', 're', 'inter_mat', 'mu', 'sigma')
-try(stan_model_check(fit = fit,
-                     results_folder = Dir.FIABiome,
-                     params = param.vec))
-# stan_post_pred_check(post.draws = joint.post.draws, # error here on missing data!!!
-#                       results_folder = Dir.Malyon,
-#                       stan.data = StanList_ls)
-# Interactions can now be divided by the appropriate scaling (intrinsic performance, and demographic rates
-# if a population dynamics model is used) in order to return per capita interaction strengths. 
-Interaction_mean <- apply(inter_mat, c(1, 2), mean) # will return the mean estimate for every interaction (NB: this is the mean of the 80% posterior interval, so will be slightly different to the mean value returned from summary(fit), which is calculated from the full posterior distribution)  
-Interaction_mean <- Interaction_mean[, order(colnames(Interaction_mean))] # sort columns
-Interaction_mean <- Interaction_mean*-1 # need to switch sign of results
-diag(Interaction_mean) <- NA
-Interaction_hpdi <- apply(inter_mat, c(1, 2), HPDI, prob = 0.89) *-1 # need to switch sign of results
-Interaction_min <- Interaction_hpdi[1,,]
-diag(Interaction_min) <- NA
-Interaction_max <- Interaction_hpdi[2,,]
-diag(Interaction_max) <- NA
-Interactions_igraph <- data.frame(Actor = rep(dimnames(Interaction_mean)$neighbour, length(dimnames(Interaction_mean)$species)),
-                                  Subject = rep(dimnames(Interaction_mean)$species, each = length(dimnames(Interaction_mean)$neighbour)),
-                                  Inter_mean = as.vector(t(Interaction_mean)),
-                                  Inter_min = as.vector(t(Interaction_min)),
-                                  Inter_max = as.vector(t(Interaction_max))
-)
-# Interactions_Malyon <- Interactions_igraph[order(abs(Interactions_igraph$Inter_mean), decreasing = TRUE), ]
-# Interactions_igraph <- na.omit(Interactions_Malyon)
-
-Interactions_DAG <- Interactions_igraph[order(Interactions_igraph$Actor), ]
-Interactions_DAG <- na.omit(Interactions_DAG)
-
-Signs_DAG <- with(Interactions_DAG, data.frame(
-  Intrer_mean = sign(Inter_mean),
-  Intrer_max = sign(Inter_max),
-  Intrer_min = sign(Inter_min)
-))
-
-Interactions_DAG <- Interactions_DAG[abs(apply(Signs_DAG, 1, sum)) == 3, ]
-
-Interactions_DAG$Actor <- gsub(x = Interactions_DAG$Actor, pattern = " ", replacement = "_")
-Interactions_DAG$Subject <- gsub(x = Interactions_DAG$Subject, pattern = " ", replacement = "_")
-
-
-## Graph Plot
-Dag_Paths <- paste(paste(Interactions_DAG$Actor, "->", Interactions_DAG$Subject), collapse = " ")
-dag <- dagitty(x = paste0("dag {", Dag_Paths, "}"))
-tidy_dag <- tidy_dagitty(dag, layout = "fr")
-tidy_dag$data$weight <- c(abs(Interactions_DAG$Inter_mean), rep(NA, sum(is.na(tidy_dag$data$direction))))
-tidy_dag$data$label <- c(round(Interactions_DAG$Inter_mean, 2), rep(NA, sum(is.na(tidy_dag$data$direction))))
-tidy_dag$data$colour <- c(ifelse(c(Interactions_DAG$Inter_mean) > 0, "green", "red"), rep("white", sum(is.na(tidy_dag$data$direction))))
-WeightMod <- 1/max(tidy_dag$data$weight, na.rm = TRUE)
-
-DAG_plot <- ggplot(tidy_dag, aes(x = x, y = y, xend = xend, yend = yend), layout = "linear") +
-  geom_dag_point(colour = "grey", shape = 18, size = 10) +
-  geom_dag_text(colour = "black", size = 2) +
-  geom_dag_edges_arc(aes(edge_colour = colour, edge_width = exp(weight)*WeightMod)) +
-  # geom_dag_edges_diagonal(aes(edge_colour = colour, edge_width = weight*WeightMod)) + 
-  labs(title = BiomeName, subtitle = "Realised Interaction (Bimler et al. Method") + 
-  theme_dag()
-ggsave(DAG_plot, units = "cm", width = 32, height = 18, filename = file.path(Dir.FIABiome, "DAG.png"))
-
-dag_data <- na.omit(tidy_dag$data)
-df <- data.frame(
-  A = dag_data$name, 
-  B = dag_data$to)
-df.g <- graph.data.frame(d = df, directed = TRUE)
-E(df.g)$weight <- dag_data$weight
-E(df.g)$label <- dag_data$label
-E(df.g)$colour <- dag_data$colour
-
-igraph_plot <- ggraph(df.g, layout = 'linear', circular = TRUE) + 
-  # geom_edge_arc(aes(col = colour, width = weight*WeightMod, alpha = ..index..)) + 
-  # scale_edge_alpha('Edge direction', guide = 'edge_direction') + 
-  geom_edge_arc(aes(col = colour, width = exp(dag_data$weight)*WeightMod), show.legend = FALSE) + 
-  scale_edge_color_manual(values = rev(unique(dag_data$colour))) +
-  geom_node_point(color = "black", size = 2) + 
-  geom_node_text(aes(label = name),  repel = TRUE)+
-  coord_fixed() + 
-  labs(title = BiomeName, subtitle = "Realised Interaction (Bimler et al. Method)") + 
-  theme_graph()
-ggsave(igraph_plot, units = "cm", width = 32, height = 32, filename = file.path(Dir.FIABiome, "IGraph.png"))
-
-diag(Interaction_mean) <- 0
-Interaction_mean[abs(sign(Interaction_mean) + sign(Interaction_min) + sign(Interaction_max)) != 3] <- 0
-
-
-
-fv.colors = colorRampPalette(c("red","white","green")) ## define the color ramp
-colorlut = fv.colors(100)[c(1,seq(50,100,length.out=99))] ## select colors to use
-
-seq(sum(Interaction_mean>0))
-
-jpeg(filename = file.path(Dir.FIABiome, "Interactions.jpeg"), units = "cm", width = 32, height = 18, res = 1000)
-pheatmap(Interaction_mean, display_numbers = T, 
-         color = c("red", "white","green"), 
-         breaks = c(min(Interaction_mean, na.rm = TRUE), -0.01, 0.01, max(Interaction_mean, na.rm = TRUE)), main = "Actors (Columns) x Subject (Rows)",
-         fontsize = 5)
-dev.off()
